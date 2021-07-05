7c8bd1f0277689ce9a375bdbbd7512d274a40e22
AntonioK180 <antonio.kasabov@gmail.com>
2021-07-05 14:50:41 +0300

    Created the first table. Fetching data from the table.

--- test.php
+++ test.php
@@ -1,6 +1,19 @@
 <?php
   include 'db_connection.php';
-  $conn = OpenCon();
-  echo "Connected Successfully";
-  CloseCon($conn);
+  $connection = OpenCon();
+  echo "Connected Successfully" ."<br>";
+
+  $sql = "select * from joboffers";
+  $result = $connection->query($sql);
+
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      echo $row['id'] ." " .$row['title'] ." " .$row['company'] ."<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
+
+  CloseCon($connection);
 ?>
