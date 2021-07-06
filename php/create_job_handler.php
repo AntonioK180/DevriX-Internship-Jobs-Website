90035c99d2b6cd7659b991dd5c1774e0d7df1176
AntonioK180 <antonio.kasabov@gmail.com>
2021-07-06 12:06:19 +0300

    Added data filtering when creating a new job offer. Should display error messages upon invalid input.

--- php/create_job_handler.php
+++ php/create_job_handler.php
@@ -1,23 +1,25 @@
 <?php
   include 'db_connection.php';
+
+
   $connection = OpenCon();
 
-
-  $job_title = mysqli_real_escape_string($connection, $_POST['job_title']);
-  $company_name = mysqli_real_escape_string($connection, $_POST['company_name']);
+  $job_title = filter_var(mysqli_real_escape_string($connection, $_POST['job_title']), FILTER_SANITIZE_STRING);         // mysqli_real_escape_string makes the string safe for the SQL query to execute; filter_var makes the input safe for the browser
+  $company_name = filter_var(mysqli_real_escape_string($connection, $_POST['company_name']), FILTER_SANITIZE_STRING);
   $salary = mysqli_real_escape_string($connection, $_POST['salary']);
-  $description = mysqli_real_escape_string($connection, $_POST['description']);
+  $description = filter_var(mysqli_real_escape_string($connection, $_POST['description']), FILTER_SANITIZE_STRING);
 
-  $sql = "INSERT INTO joboffers(title, company, salary, description) VALUES ('$job_title', '$company_name', $salary, '$description')";
+  if((!filter_var($salary, FILTER_VALIDATE_INT) === false)){
+    $sql = "INSERT INTO joboffers(title, company, salary, description) VALUES ('$job_title', '$company_name', $salary, '$description')";
 
-  if ($connection->query($sql) === TRUE) {
-    echo "New record created successfully <br>";
-  } else {
-    echo "Error: " . $sql . "<br>" . $connection->error;
+    if ($connection->query($sql) === TRUE) {
+      echo "New record created successfully <br>";
+    } else {
+      echo "Error: " . $sql . "<br>" . $connection->error;
+    }
   }
 
   CloseCon($connection);
-
 
   header('Location: ' . '/DevriX-Internship-Jobs-Website', true, 303);
   die();
