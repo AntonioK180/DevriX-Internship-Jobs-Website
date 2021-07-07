589438611f0d98e556edf8594ac73987722919f4
AntonioK180 <antonio.kasabov@gmail.com>
2021-07-07 17:57:29 +0300

    Updated the Install & Run instructions.

--- db_config.sql
+++ db_config.sql
@@ -8,7 +8,8 @@ CREATE TABLE joboffers(
   title varchar(150) NOT NULL,
   description varchar(600) DEFAULT NULL,
   company varchar(50) NOT NULL,
-  salary int NOT NULL
+  salary int NOT NULL,
+  activated BOOLEAN DEFAULT 0
 )
 
 
