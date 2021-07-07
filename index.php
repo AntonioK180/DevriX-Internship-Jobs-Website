1744104b58b623cad4a6cf348db15ff4b0684a89
AntonioK180 <antonio.kasabov@gmail.com>
2021-07-07 10:54:52 +0300

    All basic functionalities are ready. Added the search by keyword feature.

--- index.php
+++ index.php
@@ -16,6 +16,14 @@
 			<h1 class="site-title"><a href="#">Job Offers</a></h1>
     </header>
     <a href="./php/create_job.php" id="new_offer"> Create a new offer </a>
+		<form action="" method="post">
+			<label>Kyeword: </label><input type="text" name="keyword"><br>
+			<input type="submit" name="submit_btn" value="Search">
+		</form>
+
+		<form action="" method="post">
+			<button type="submit" name="clear_btn">Clear Keyword</button>
+		</form>
 
     <?php
     include 'php/db_connection.php';
@@ -25,22 +33,49 @@
 		session_start();
     $connection = OpenCon();
 
-    $sql = "select * from joboffers";
-    $result = $connection->query($sql);
 
+		if(isset($_REQUEST['submit_btn'])){
+			$keyword =  filter_var(mysqli_real_escape_string($connection, $_POST['keyword']), FILTER_SANITIZE_STRING);
+			header('Location: ' .'/DevriX-Internship-Jobs-Website/index.php?keyword=' .$keyword, true, 303);
+		}
 
-    if($result->num_rows > 0){
-      while($row = $result->fetch_assoc()){
-        $id = $row['id'];
-        $job_title = $row['title'];
-        $company_name = $row['company'];
+		if(isset($_REQUEST['clear_btn'])){
+			header('Location: ' .'/DevriX-Internship-Jobs-Website/index.php', true, 303);
+		}
 
-        echo indexPageOffersParser($id, $job_title, $company_name);
+		if(isset($_GET['keyword'])){
+	    $keyword =  filter_var(mysqli_real_escape_string($connection, $_GET['keyword']), FILTER_SANITIZE_STRING);
 
-      }
-    } else {
-      echo "0 results";
-    }
+			$sql_keyword = "SELECT * FROM joboffers WHERE description LIKE '%" .$keyword ."%' OR title LIKE '%" .$keyword ."%'";
+			if($result_keyword = $connection->query($sql_keyword)){
+				if($result_keyword->num_rows > 0){
+					while($row = $result_keyword->fetch_assoc()){
+						$id = $row['id'];
+						$job_title = $row['title'];
+						$company_name = $row['company'];
+
+						echo indexPageOffersParser($id, $job_title, $company_name);
+					}
+				} else {
+					echo "0 results";
+				}
+			}
+		} else {
+			$sql = "select * from joboffers";
+			if($result = $connection->query($sql)){
+				if($result->num_rows > 0){
+					while($row = $result->fetch_assoc()){
+						$id = $row['id'];
+						$job_title = $row['title'];
+						$company_name = $row['company'];
+
+						echo indexPageOffersParser($id, $job_title, $company_name);
+					}
+				} else {
+					echo "0 results";
+				}
+			}
+		}
 
 		CloseCon($connection);
     ?>
